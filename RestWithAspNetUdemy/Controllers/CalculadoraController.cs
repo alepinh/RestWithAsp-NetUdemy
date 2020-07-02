e0c2576b36ce65c03732a0c82630f70874104616
Adm Alepinh <alexandre.louko@gmail.com>
2020-07-02 01:23:47 -0300

    Calculadora Rest

---
+++ RestWithAspNetUdemy/Controllers/CalculadoraController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestWithAspNetUdemy.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculadoraController : ControllerBase
+    {
+      
+        [HttpGet("Soma/{PrimeiroNumero}/{SegundoNumero}")]
+        public IActionResult Soma(string primeiroNumero, string segundoNumero)
+        {
+            if (Isnumeric(primeiroNumero) && Isnumeric(segundoNumero))
+            {
+                var sum = ConverterParaDecimal(primeiroNumero) + ConverterParaDecimal(segundoNumero);
+                return Ok(sum.ToString());
+            
+            }
+
+            return BadRequest("Entrada inválida");
+        }
+
+        private decimal ConverterParaDecimal(string Numero)
+        {
+            decimal decimalValue;
+            if(decimal.TryParse(Numero, out decimalValue))
+            {
+                return decimalValue;
+            }
+
+            return 0;
+        }
+
+        private bool Isnumeric(string stringNumero)
+        {
+            double number;
+            bool isNumber = double.TryParse(stringNumero, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
