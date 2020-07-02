c3c83761be1546c18cdcd857a01c59493245f03e
Adm Alepinh <alexandre.louko@gmail.com>
2020-07-02 11:47:45 -0300

    Aprendendo outros verbos - Aula 2

---
+++ RestWithAspNetUdemy/Model/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithAspNetUdemy.Model
+{
+    public class Pessoa
+    {
+        public long id { get; set; }
+        public string Nome { get; set; }
+        public string sobreNome { get; set; }
+        public string Endereceo { get; set; }
+        public string Genereo { get; set; }
+
+
+    }
+}
